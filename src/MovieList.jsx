cada65da74e9cc283c2d146daebbf895b4e4840d
DorraDray <60601319+DorraDray@users.noreply.github.com>
2020-03-23 13:10:25 +0100

    first commit

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+// import MovieList from './MovieList'
+import HOC from './HOC';
+
+
+
+
 class MovieList extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
     render() { 
+    
+    
+
         return ( <div className="Movie" >
                     {this.props.MovieList.filter(el=>(el.name.toLowerCase().includes(this.props.searchInput.toLowerCase().trim()))
-                    &&(el.rating>=this.props.searchStar)).map(el=> <div  className="Card">
+                    &&(el.rating<=this.props.searchStar)).map(el=> <div  className="Card">
                         <h1> {el.name}</h1>
                         <span>{"".padStart(el.rating,"⭐")}</span>
                         <img className="img" src= {el.image}/>
@@ -26,4 +35,4 @@ class MovieList extends Component {
     }
 }
  
-export default MovieList ;
+export default HOC(MovieList) ;
